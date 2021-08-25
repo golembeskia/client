0bba0418c9574d8e74cfde828c1aef7fe9eecf98
golembeskia <68037849+golembeskia@users.noreply.github.com>
2021-08-25 09:29:58 -0400

    Update Cart.js
    Re-order the titles

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -40,15 +40,15 @@ const Cart = () => {
       <thead className="thead-light">
         <tr>
           <th scope="col">Image</th>
+          <th scope="col">Brand</th>
           <th scope="col">Title</th>
           <th scope="col">Price</th>
-          <th scope="col">Brand</th>
           {/* <th scope="col">Color</th>
           <th scope="col">Count</th>
           <th scope="col">Shipping</th> */}
           <th scope="col">Diet</th>
-          <th scope="col">Category</th>
-          <th scope="col">Sub Category</th>
+          {/* <th scope="col">Category</th>
+          <th scope="col">Sub Category</th> */}
           <th scope="col">Remove</th>
         </tr>
       </thead>
