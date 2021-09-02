6c4fb3a51a044d638db902b615048eeb8a0dcbab
golembeskia <68037849+golembeskia@users.noreply.github.com>
2021-09-02 10:33:21 -0400

    Update Star.js
    Updated so when you click on star it turns red.

--- src/components/forms/Star.js
+++ src/components/forms/Star.js
@@ -1,18 +1,38 @@
-import React from 'react'
+// import React from "react";
+// import StarRating from "react-star-ratings";
+
+// const Star = ({ starClick, numberOfStars }) => (
+//   <>
+//     <StarRating
+//       changeRating={() => starClick(numberOfStars)}
+//       numberOfStars={numberOfStars}
+//       starDimension="20px"
+//       starSpacing="2px"
+//       starHoverColor="#fadb14"
+//       starEmptyColor="#fadb14"
+//     />
+//     <br />
+//   </>
+// );
+
+// export default Star;
+
+  
+import React, {Fragment} from 'react'
 import StarRating from 'react-star-ratings'
 
-const Star = ({ starClick, numberOfStars }) => (
-  <>
-    <StarRating
-      changeRating={() => starClick(numberOfStars)}
-      numberOfStars={numberOfStars}
-      starDimension="20px"
-      starSpacing="2px"
-      starHoverColor="#fadb14"
-      starEmptyColor="#fadb14"
-    />
-    <br />
-  </>
+const Star = ({starClick, num, starEmptyColor='#fadb14'}) => (
+    <Fragment>
+        <StarRating
+        changeRating={() => starClick(num.starNum)}
+        numberOfStars={num.starNum}
+        starDimension="20px"
+        starSpacing="2px"
+        starEmptyColor={starEmptyColor}
+        starHoverColor={starEmptyColor}
+        editing={false}
+        />
+    </Fragment>
 )
 
 export default Star
