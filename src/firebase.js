970a8ee557d2dd70431bae4f2f05b79070a371fc
Travis Falls <369837+N3rd007@users.noreply.github.com>
2021-09-03 09:09:33 -0400

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase/app'
+import * as firebase from 'firebase'
 import 'firebase/auth'
+import 'firebase/app'
 
 // Your web app's Firebase configuration
 const config = {
